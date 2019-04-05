3d2aa7d1a1055f9dfe3a03411c1502cb395b86cb
ynnckth <yannickstreit@hotmail.com>
2019-04-05 11:15:34 +0200

    Implement hamcrest matchers exercise

--- hamcrest/src/test/java/com/zuehlke/testing/hamcrest/exercises/CollectionTest.java
+++ hamcrest/src/test/java/com/zuehlke/testing/hamcrest/exercises/CollectionTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import com.zuehlke.testing.hamcrest.Person;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class CollectionTest {
 
 	@Test
@@ -16,8 +20,19 @@ public class CollectionTest {
 
 		// assert
 		// 1. assert 3 is present
+		assertThat(values, hasItem(3));
+
 		// 2. assert 1,3,4,2,5 are in the list
+		assertAll(
+				() -> assertThat(values, hasItem(1)),
+				() -> assertThat(values, hasItem(2)),
+				() -> assertThat(values, hasItem(3)),
+				() -> assertThat(values, hasItem(4)),
+				() -> assertThat(values, hasItem(5))
+		);
+
 		// 3. assert 1,2,3,4,5 are in the list
+		assertThat(values, hasItems(2,3,5,1,4));
 	}
 
 	@Test
@@ -29,8 +44,15 @@ public class CollectionTest {
 
 		// assert
 		// 1. assert fritz is present
+		assertThat(values, hasItem(fritz));
+
 		// 2. assert new Person(2, "Fritz", "Meier", 5) is present
+		assertThat(values, hasItem(samePropertyValuesAs(new Person(2, "Fritz", "Meier", 5))));
+
 		// 3. assert peter, fritz are in the list
+		assertThat(values, contains(peter, fritz));
+
 		// 4. assert fritz, peter are in the list
+		assertThat(values, containsInAnyOrder(fritz, peter));
 	}
 }
